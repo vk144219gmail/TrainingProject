8df1e66e1e6143d65d7e5bd5ef102f2a8ba2c093
vk144219gmail <vk144219@gmail.com>
2022-08-09 16:31:46 +0530

    first commit

---
+++ src/test/java/sample/TestClass.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+		@Test
+		public void testcase1() {
+			
+		
+		System.out.println("vishal Babu Kamble");
+	
+		}
+	
+
+}
